1241b408bd321270353334d60a6402276d59ec3a
Nikhil <nikhil@tartanhq.com>
2023-12-04 18:42:51 +0530

    icon

--- background.js
+++ background.js
@@ -5,9 +5,26 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // Log the message from content.js
-  // console.log(request?.data);
-
-  // You can send a response back to content.js if needed
-  // sendResponse({ backgroundMessage: "Message received in background.js!" });
+  function verifyPan(pan) {
+    const payload = {
+      category: "individual-pii-data",
+      type: "pan-detail-v2",
+      applicationId: "Dashboard-realtime-KYC",
+      data: { panNumber: pan },
+      mode: "PROD",
+    };
+    const endpoint = `https://api-prod.tartanhq.com/aphrodite/api/tp/v1/verification`;
+    const token = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRpa3N0YWdlZmFzdHRyYWNrIiwiZXhwIjoxNzAxNzAxNDI1LCJpYXQiOjE3MDE2ODM0MjV9.u0xszsidwoeqnVOONTnbgx2QbcNHDKcWlcmh86RBlA8Tv5WtlkfmaIu5nyYEVgyCWSNRdTIfg41VW15eHUVqew`;
+    return fetch(endpoint, payload, { headers: token })
+      .then((data) => data.json())
+      .catch((error) => {
+        console.log(error);
+        return false;
+      })
+      .then((response) => {
+        return response && response.response && response.response.isValid;
+      });
+  }
+  console.log(request?.data);
+  verifyPan(request?.data);
 });
--- index.js
+++ index.js
@@ -10,13 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
       chrome.scripting.executeScript({
         target: { tabId: activeTab.id },
-        function: extractDOM,
+        function: init,
       });
     });
   });
 });
 
-function extractDOM() {
+function init() {
+
+  // Network call for PAN
   function verifyPan(pan) {
     const payload = {
       category: "individual-pii-data",
@@ -26,38 +28,71 @@ function extractDOM() {
       mode: "PROD",
     };
     const endpoint = `https://api-prod.tartanhq.com/aphrodite/api/tp/v1/verification`;
-    const token = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRpa3N0YWdlZmFzdHRyYWNrIiwiZXhwIjoxNzAxNzAxNDI1LCJpYXQiOjE3MDE2ODM0MjV9.u0xszsidwoeqnVOONTnbgx2QbcNHDKcWlcmh86RBlA8Tv5WtlkfmaIu5nyYEVgyCWSNRdTIfg41VW15eHUVqew`;
-    return fetch(endpoint, payload, { headers: token })
-      .then((data) => data.json())
-      .catch((error) => {
+    const token = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRpa3N0YWdlZmFzdHRyYWNrIiwiZXhwIjoxNzAxNzAxNDI1LCJpYXQiOjE3MDE2ODM0MjV9.u0xszsidwoeqnVOONTnbgx2QbcNHDKcWlcmh86RBlA8Tv5WtlkfmaIu5nyYEVgyCWSNRdTIfg41VW15eHUVqew.eyJzdWIiOiJiYXRpa3N0YWdlZmFzdHRyYWNrIiwiZXhwIjoxNzAxNzAxNDI1LCJpYXQiOjE3MDE2ODM0MjV9.u0xszsidwoeqnVOONTnbgx2QbcNHDKcWlcmh86RBlA8Tv5WtlkfmaIu5nyYEVgyCWSNRdTIfg41VW15eHUVqew`;
+    return fetch(endpoint, {
+      method: "POST",
+      mode: "cors",
+      data: JSON.stringify(payload),
+      headers: {
+        "Authorization": token
+      }
+    })
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .catch((error) => {
         console.log(error);
         return false;
-      })
-      .then((response) => {
+    })
+    .then((response) => {
+        console.log("[response]", response);
         return response && response.response && response.response.isValid;
-      });
+    });
+    // return fetch(endpoint, payload, { headers: token })
+    //   .then((data) => data.json())
+    //   .catch((error) => {
+    //       console.log(error);
+    //       return false;
+    //   })
+    //   .then((response) => {
+    //       console.log("[response]", response);
+    //       return response && response.response && response.response.isValid;
+    //   });
   }
-  const unVerifiedIcon =
-    "https://res.cloudinary.com/dzfkhe75f/image/upload/v1701683698/cursor-click-02-svgrepo-com_tf24yk.svg";
-  const verifedIcon =
-    "https://res.cloudinary.com/dzfkhe75f/image/upload/v1701683393/verify-svgrepo-com_1_xd4tbu.svg";
-  const selector = `span.ng-scope.ng-binding[ng-switch-when="masked"]`;
-  const element = document.querySelector(selector);
-  const panRegex = /[A-Z]{5}[0-9]{4}[A-Z]{1}/;
-  const panMatches = document?.body?.innerHTML?.match(panRegex)?.[0];
-  const span = document.createElement("span");
-  span.addEventListener("click", function () {
-    verifyPan(panMatches);
-  });
-  span.style.cursor = "pointer";
-  const iconImage = document.createElement("img");
-  iconImage.src = unVerifiedIcon;
-  iconImage.alt = "Unverified Icon";
-  iconImage.width = 30;
-  iconImage.height = 30;
 
-  span.appendChild(iconImage);
-  element.appendChild(span);
+  // Parse DOM
+  function extractDOM() {
+    const unVerifiedIcon =
+      "https://res.cloudinary.com/dzfkhe75f/image/upload/v1701683698/cursor-click-02-svgrepo-com_tf24yk.svg";
+    const verifedIcon =
+      "https://res.cloudinary.com/dzfkhe75f/image/upload/v1701683393/verify-svgrepo-com_1_xd4tbu.svg";
+    const selector = `span.ng-scope.ng-binding[ng-switch-when="masked"]`;
+    const element = document.querySelector(selector);
+    const panRegex = /[A-Z]{5}[0-9]{4}[A-Z]{1}/;
+    const panMatches = document?.body?.innerHTML?.match(panRegex)?.[0];
+    const span = document.createElement("span");
+    // span.addEventListener("click", function () {
+    //   chrome.runtime.sendMessage({ action: "domDetails", data: panMatches });
+    // });
+    span.addEventListener("click", function () {
+      verifyPan(panMatches);
+    });
+    span.style.cursor = "pointer";
+    const iconImage = document.createElement("img");
+    iconImage.src = unVerifiedIcon;
+    iconImage.alt = "Unverified Icon";
+    iconImage.width = 30;
+    iconImage.height = 30;
+  
+    span.appendChild(iconImage);
+    element.appendChild(span);
+  }
+
+  extractDOM();
+
 }
+
+
 
 // chrome.runtime.sendMessage({ action: "domDetails", data: elementWithPan });
